18c8375409704d4f3e8f92653b954e71ee253ed8
Alvarospereira <alvarodasilva.pereira@gmail.com>
2022-03-16 12:21:07 -0300

    fonteawesome

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,4 +1,5 @@
-// Entry point for the build script in your package.json
+// Entry point for the build script in your package.json  -- import "@fortawesome/fontawesome-free/css/all"
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
