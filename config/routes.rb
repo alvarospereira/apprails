d09231922413a57383644a59ea71b2d623837ae9
Alvarospereira <alvarodasilva.pereira@gmail.com>
2022-03-18 18:06:33 -0300

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'static_public#landing_page'
   get 'privacy', to: 'static_public#privacy'
   get 'terms', to: 'static_public#terms'
+
+  resources :users, only: %i[index shown]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
